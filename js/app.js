6b6ff035815268e0eceabc0dbdb864d37456eb1c
Catalin <ene.catalinionut@yahoo.com>
2018-07-01 16:24:34 +0300

    First commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+import Playlist from './modules/playlist.js';
+import PlayInfo from './modules/play-info.js';
+import TrackBar from './modules/track-bar.js';
+Playlist.init();
+PlayInfo.init();
+TrackBar.init();
---
+++ js/data/songs.js
@@ -0,0 +1,85 @@
+export const songsList = [{
+    "id": "1",
+    "url": "../../music/1.mp3",
+    "time": "02:58",
+    "title": "Eu",
+    "artist": "DOC"
+  },
+  {
+    "id": "2",
+    "url": "../../music/2.mp3",
+    "time": "04:02",
+    "title": "Luca 18_16",
+    "artist": "DOC,Nwanda,Grasu XXL"
+  },
+  {
+    "id": "3",
+    "url": "../../music/3.mp3",
+    "time": "03:47",
+    "title": "SUD",
+    "artist": "DOC"
+  },
+  {
+    "id": "4",
+    "url": "../../music/4.mp3",
+    "time": "02:53",
+    "title": "I Can Feel",
+    "artist": "Ela Rose, David Deejay"
+  },
+  {
+    "id": "5",
+    "url": "../../music/5.mp3",
+    "time": "03:16",
+    "title": "Lista de pacate",
+    "artist": "Liviu Teodorescu, Killa Fonic"
+  },
+  {
+    "id": "6",
+    "url": "../../music/6.mp3",
+    "time": "03:06",
+    "title": "Ma doare la Bass",
+    "artist": "Marius Moga"
+  },
+  {
+    "id": "7",
+    "url": "../../music/7.mp3",
+    "time": "03:51",
+    "title": "Pana imi Iese",
+    "artist": "Neli"
+  },
+  {
+    "id": "8",
+    "url": "../../music/8.mp3",
+    "time": "04:27",
+    "title": "Parfum",
+    "artist": "Neli"
+  },
+  {
+    "id": "9",
+    "url": "../../music/9.mp3",
+    "time": "03:57",
+    "title": "Post Malone Psycho Ft. Ty Dolla",
+    "artist": "Post malone"
+  },
+  {
+    "id": "10",
+    "url": "../../music/10.mp3",
+    "time": "03:40",
+    "title": "Congratulations ft. Quavo",
+    "artist": "Post Malone"
+  },
+  {
+    "id": "11",
+    "url": "../../music/1.mp3",
+    "time": "04:01",
+    "title": "Rockstar",
+    "artist": "Post Malone"
+  },
+  {
+    "id": "12",
+    "url": "../../music/2.mp3",
+    "time": "02:46",
+    "title": "Sad",
+    "artist": "XXXtentacion"
+  },
+];
